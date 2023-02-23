85b129837fea8e9de689c671d1ea2734804e3220
Luis Florez <braian1k3@gmail.com>
2023-02-23 11:14:55 -0500

    Crear nuevo proyecto para practicar herencia mediante un ejemplo de familia.

---
+++ Herencia/src/main/java/org/example/FamiliaArdilla.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class FamiliaArdilla {
+    public static void main(String[] args) {
+        Padre padre = new Padre();
+        padre.setNombre("Chape");
+        padre.setFortuna(150L);
+
+        Hijo hijo = new Hijo();
+        hijo.setNombre("Andres");
+        hijo.setFortuna(300L);
+
+        System.out.println("El nombre del papa es " +
+                padre.getNombre() + " " + padre.getApellido() +
+                    " y su fortuna es de $" + padre.getFortuna());
+        System.out.println("El nombre del hijo es " +
+                hijo.getNombre() + " " + hijo.getApellido() +
+                " y su fortuna es de $" + hijo.getFortuna());
+    }
+}
