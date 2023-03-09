38858bf4e36c1f9faac5961bace2989ddd869636
Luis Florez <braian1k3@gmail.com>
2023-03-09 10:27:46 -0500

    Empezar a darle atributos y métodos a cada clase, además de ver por medio del main el funcionamiento de dichas clases.

--- DragonBall/src/main/java/org/example/ClaseBaja.java
+++ DragonBall/src/main/java/org/example/ClaseBaja.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class ClaseBaja {
+import java.text.MessageFormat;
+
+public class ClaseBaja extends Sayayin {
+
+    public ClaseBaja() {
+    }
+
+    public ClaseBaja(String nombre, Integer poderPelea, Integer edad, String colorPelo) {
+        super(nombre, poderPelea, edad, colorPelo);
+    }
+
+    @Override
+    public void pelea() {
+        System.out.println(MessageFormat.format("{0} pelea con el corazón.", this.getNombre()));
+    }
 }
--- DragonBall/src/main/java/org/example/PlanetaSayayin.java
+++ DragonBall/src/main/java/org/example/PlanetaSayayin.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class PlanetaSayayin {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClaseAlta guerrero1 = new ClaseAlta("Vegget", 52, 100, "Negro");
+        ClaseBaja guerrero2 = new ClaseBaja("Goku", 77, 80, "Negro");
+
+        System.out.println(guerrero1);
+        guerrero1.pelea();
+        System.out.println(guerrero2);
+        guerrero2.pelea();
     }
 }
