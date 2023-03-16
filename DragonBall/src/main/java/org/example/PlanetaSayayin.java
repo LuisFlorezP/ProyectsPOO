c39dcebed2186994acee2018d088e403ca089309
Luis Florez <braian1k3@gmail.com>
2023-03-15 23:39:57 -0500

    Corregir sintáxis.

--- DragonBall/src/main/java/org/example/PlanetaSayayin.java
+++ DragonBall/src/main/java/org/example/PlanetaSayayin.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class PlanetaSayayin {
     public static void main(String[] args) {
-        ClaseAlta guerrero1 = new ClaseAlta("Vegget", 52, 100, "Negro");
+        ClaseAlta guerrero1 = new ClaseAlta("Veggeta", 52, 100, "Negro");
         ClaseBaja guerrero2 = new ClaseBaja("Goku", 77, 80, "Negro");
 
         System.out.println(guerrero1);
